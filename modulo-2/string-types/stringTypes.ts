8ddd02b5e9ca039a1633141b7668246aa1d7b6c8
julio-kuniwake <julio.jtkuniwake@gmail.com>
2023-03-09 15:43:22 -0300

    Type string

---
+++ modulo-2/string-types/stringTypes.ts
@@ -0,0 +1,12 @@
+// ************ Single Quotes ************
+let nomeCompleto: string = 'Julio Kuniwake';
+console.log(nomeCompleto);
+
+// ************ Double Quotes ************
+let funcaoEmpresa: string = "Desenvolvedor";
+console.log(funcaoEmpresa);
+
+// ************ Back Ticks ************
+let nomeEmpresa: string = 'EY';
+let dadosFuncionario: string = `Seja bem vindo ${nomeCompleto}! Voce será ${funcaoEmpresa}, na empresa ${nomeEmpresa}!`;
+console.log(dadosFuncionario);
