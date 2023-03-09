aab5c9405de3e19893939ab446a2c96e8659d8ad
julio-kuniwake <julio.jtkuniwake@gmail.com>
2023-03-09 14:20:07 -0300

    Type boolean

---
+++ modulo-2/boolean-types/booleanTypes.ts
@@ -0,0 +1,16 @@
+// ************ Exemplo 1 ************
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+
+// ************ Exemplo 2 ************
+// O ! nega a condição
+let concluido: boolean = false;
+
+if (!concluido) {
+  console.log("Terefa concluida com sucesso!");
+} else {
+  console.log("Tarefa pendente!");
+}
