d8d84915bc9dc8cb6fdac2eccb90d6ca2adc3860
julio-kuniwake <julio.jtkuniwake@gmail.com>
2023-03-10 15:19:52 -0300

    Type null e undefined

---
+++ modulo-2/null-undefined-type/nullUndefinedType.ts
@@ -0,0 +1,14 @@
+// ************ null ************
+let valueNull = null;
+console.log(null);
+console.log(typeof (valueNull))
+
+// ************ undefined ************
+let valueUndefined;
+
+console.log(valueUndefined);
+console.log(typeof (valueUndefined));
+
+// ************ Diferenças e similaridades null vs undefined ************
+console.log('Os resultados são iguais? ', null == undefined);
+console.log('Os valores e o tipo são iguais? ', null === undefined); // Da errado pois: null é um objeto, e undefined é undefined
